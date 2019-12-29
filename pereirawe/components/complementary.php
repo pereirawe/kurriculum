b2f0218f4c6680b3aa89ad63f30645a9c8d0880f
William Enrique Pereira Carrasquero <pereirawe@gmail.com>
2019-12-28 21:02:11 -0300

    WIP Initial Commit

---
+++ pereirawe/components/complementary.php
@@ -0,0 +1,40 @@
+<div class="container-fluid pb-5" id="complementary">
+        <div class="row">
+            <div id="title" class="col-md-12">
+                <h2 class="pt-5 pb-2"><i class="fas fa-star "></i> <?= $appContent->appAditionalEducationTitle;?></h2>
+            </div>
+
+            <div class="col-md-4 p-3">
+                <div class="p-3 bgLight">
+                    <h5>IDIOMAS</h5>
+                    <ul>
+                        <li>Espanhol Nativo</li>
+                        <li>Inglês Avançado</li>
+                        <li>Português Intermédio</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-4 p-3">
+                <div class="p-3 bgLight">
+                    <h5>SISTEMAS DE COMPUTADORES</h5>
+                    <ul>
+                        <li>Windows</li>
+                        <li>MacOs</li>
+                        <li>Ubuntu</li>
+                        <li>Office</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-4 p-3">
+                <div class="p-3 bgLight">
+                    <h5>PRODUÇÃO AUDIOVISUAL E MARKETING</h5>
+                    <ul>
+                        <li>Edição de Áudio e Vídeo</li>
+                        <li>Retoque de imagem digital</li>
+                        <li>Gestão de Redes Sociais</li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    </div>
